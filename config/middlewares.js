2ee06169c81ee4845424c3f5bd7da133f1aaeaeb
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2022-06-23 16:50:55 +0300

    revert last push

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,7 +14,7 @@ module.exports = [
     config: {
       enabled: true,
       header: '*',
-      origin: ['http://localhost:3000','http://localhost:1337','https://nextjsstrapi.herokuapp.com']
+      origin: ['http://localhost:3000','http://localhost:1337','https://smiple-ecommerce-strapi.herokuapp.com']
     }
   }
 ];
